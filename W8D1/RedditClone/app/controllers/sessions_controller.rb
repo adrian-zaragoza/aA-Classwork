32f787c5ea650d833b608b119561d4c9d20037ed
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-03-08 10:09:05 -0800

    Complete view for new Session

--- W8D1/RedditClone/app/controllers/sessions_controller.rb
+++ W8D1/RedditClone/app/controllers/sessions_controller.rb
@@ -5,7 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by_credentials()
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+        if @user
+            login(@user)
+            @current_user = @user
+            redirect_to users_url
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        logout
+        redirect_to new_session_url
     end
 
    
