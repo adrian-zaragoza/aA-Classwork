fafb0af622f3cfcd45788c0e469064c0a5c314c7
Gino Reyes <grmreyes@gmail.com>
2021-03-09 01:51:08 +0800

    Wrote application controller

--- W8D1/RedditClone/app/controllers/sessions_controller.rb
+++ W8D1/RedditClone/app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials()
+    end
+
+   
+
 end
--- W8D1/RedditClone/app/controllers/users_controller.rb
+++ W8D1/RedditClone/app/controllers/users_controller.rb
@@ -3,5 +3,21 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user && @user.save
+      login(@user)
+      redirect_to users_url
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end
+
   
 end
