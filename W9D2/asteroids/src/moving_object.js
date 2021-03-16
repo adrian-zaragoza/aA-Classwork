8f0b0c151070d413b0862671d997decf9c27bf49
mike <michaeldeanmdmph@gmail.com>
2021-03-16 13:32:30 -0700

    moving object draw

--- W9D2/asteroids/src/moving_object.js
+++ W9D2/asteroids/src/moving_object.js
@@ -5,4 +5,20 @@ this.radius = options["radius"]
 this.color = options["color"]
 };
 
+MovingObject.prototype.Draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+  
+    ctx.arc(
+      this.pos[0],
+      this.pos[1],
+      this.radius,
+      0,
+      2 * Math.PI,
+      false
+    );
+  
+    ctx.fill();
+
+}
 module.exports = MovingObject;
