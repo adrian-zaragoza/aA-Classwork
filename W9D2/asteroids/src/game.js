df09bcae1a514bfdfe9ecd124286c4dfba6f379f
mike <michaeldeanmdmph@gmail.com>
2021-03-16 16:37:11 -0700

    game.draw

--- W9D2/asteroids/src/game.js
+++ W9D2/asteroids/src/game.js
@@ -2,12 +2,27 @@ const DIM_X = 480;
 const DIM_Y = 640;
 const NUM_ASTEROIDS = 20;
 
+function Game(){
+  this.asteroids = this.addAsteroids();
+}
 Game.prototype.addAsteroids = function(){
   const asteroids = [];
-  while(NUM_ASTEROIDS > 0){
-    asteroids.push(new Asteroids(this.randomPosition()))
+  while(NUM_ASTEROIDS >= asteroids.length){
+    asteroids.push(new Asteroids(this.randomPosition(), asteroids.length))
   }
+  return asteroids;
 }
+
 Game.prototype.randomPosition = function(){
+  let randomX =  Math.floor(Math.random() * DIM_X); 
+  let randomY =  Math.floor(Math.random() * DIM_Y);
+  return [randomX, randomY]; 
+}
+
+Game.prototype.draw(ctx) = function(){
+  ctx.clearRect(ctx.pos[0], ctx.pos[1], DIM_X, DIM_Y);
+  for (let i = 0; i < this.asteroids.length; i++) {
+    this.asteroids[i].draw(ctx);
+  }
 
 }
