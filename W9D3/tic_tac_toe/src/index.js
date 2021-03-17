a236be8a862145130ba4aca0c8c119f9d5fb77f8
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-03-17 15:00:47 -0700

    build gird

--- W9D3/tic_tac_toe/src/index.js
+++ W9D3/tic_tac_toe/src/index.js
@@ -2,10 +2,11 @@ const View = require('./ttt-view.js');
 const Game = require('./game.js');
 
   window.Game = Game;
-  
+
 
   $(() => {
     // Your code here
+    console.log("inside the index")
     const rootEle = $('.ttt');
     const game = new Game();
     new View(game, rootEle);
--- W9D3/tic_tac_toe/src/ttt-view.js
+++ W9D3/tic_tac_toe/src/ttt-view.js
@@ -1,16 +1,32 @@
 class View {
-  constructor(game, $el) {};
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+    console.log("I'm inside the constructor")
+  };
 
   bindEvents() {};
 
   makeMove($square) {};
 
   setupBoard() {
-    grid = new Array(3);
-    $('<ul class = "grid"></ul>')
-    for (let i = 0; i < 10; i++) {
-      $('<li class = "cell"></li>')
-    };
+    // grid = new Array(3);
+    // $('<ul class = "grid"></ul>')
+    // for (let i = 0; i < 10; i++) {
+    //   $('<li class = "cell"></li>')
+    // };
+    let $ul = $('<ul class="grid"></ul>');
+
+    for(let row = 0; row < 3; row++){
+      for (let col = 0; col < 3; col++){
+        let $li = $('<li class="cell"></li>')
+        $li.data("pos", [row, col]);
+        $ul.append($li);
+      }
+    }
+    this.$el.append($ul);
+    console.log("Inside the setupBoard")
   };
 
 
