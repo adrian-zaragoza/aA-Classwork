79b9ffb98f542b8a6e3b4ffe181b0111cebd40cd
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-03-17 17:31:44 -0700

    Finish Tic Tac Toe

--- W9D3/tic_tac_toe/src/ttt-view.js
+++ W9D3/tic_tac_toe/src/ttt-view.js
@@ -6,19 +6,41 @@ class View {
     this.game = game;
     this.$el = $el;
     this.setupBoard();
-    console.log("I'm inside the constructor")
+    this.bindEvents();
+    // console.log("I'm inside the constructor")
   };
 
   bindEvents() {
-    this.$li.on('click', () => {
-      this.game.playMove(pos);
-      $li.addClass('ul > .click');
-    });
+    this.$el.on("click", "li", (e => {
+      let $square = $(e.currentTarget);
+      // this.game.playMove(pos);
+      $square.addClass("click");
+      this.makeMove($square);
+    }));
     //how do we know x vs o
+
     //how do we create a click event that stays once clicked (:after classes)
   };
 
-  makeMove($square) {};
+  makeMove($square) {
+    let currentPlayer = this.game.currentPlayer;
+    let pos = $square.data("pos")
+    this.game.playMove(pos)
+    $square.addClass(currentPlayer);
+
+    if(this.game.isOver()){
+      this.$el.off("click");
+
+      let winner = this.game.winner();
+      if(winner){
+        this.$el.addClass(`winner-${winner}`);
+        this.$el.append(`${winner} is the Winner`);
+      }else{
+        this.$el.append(`It's a draw!`);
+      }
+      this.$el.addClass("game-over");
+    }
+  };
 
   setupBoard() {
 
@@ -26,7 +48,7 @@ class View {
 
     for(let row = 0; row < 3; row++){
       for (let col = 0; col < 3; col++){
-        let $li = $('<li class="cell"></li>')
+        let $li = $('<li></li>')
         $li.data("pos", [row, col]);
         $ul.append($li);
       }
