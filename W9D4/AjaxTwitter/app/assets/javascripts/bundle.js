31ed13c889776009f12da925686eea2fce9f4b25
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-03-18 22:43:37 -0700

    Add Twitter project to repo

---
+++ W9D4/AjaxTwitter/app/assets/javascripts/bundle.js
@@ -0,0 +1,23 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************!*\
+  !*** ./frontend/twitter.js ***!
+  \*****************************/
+const FollowToggle = required('./follow_toggle.js');
+
+$(()=>{
+  $('.follow-toggle').each((idx, button)=>{
+    new FollowToggle(button); // we are reusing this code for another action where we display all the buttons on the window.
+  })
+})
+
+
+  // $('.follow-toggle').on('click', ((event)=>{
+
+//   event.preventDefault();
+//   let follow = new FollowToggle(); //pass the button
+//   return follow;
+// }))
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
