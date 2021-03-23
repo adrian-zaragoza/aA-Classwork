caf1366704fc45b3fdf8fb29f46dce5eca85d5bc
jason <jmasters1000@yahoo.com>
2021-03-23 16:50:25 -0700

    Work on clock class

--- W10D3/widgets/frontend/clock.jsx
+++ W10D3/widgets/frontend/clock.jsx
@@ -6,15 +6,31 @@ class Clock extends React.Component{
     this.state = {
       time: new Date()
     }
+    this.interval = null;
+    this.tick = this.tick.bind(this)
   }
+
   tick(){
     this.setState({ time: new Date()})
   }
 
+  componentDidMount() {
+    this.interval = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
   render(){
+    let hours = this.state.time.getHours()
+    let minutes = this.state.time.getMinutes()
+    let seconds = this.state.time.getSeconds()
     return(
     <div>
       <h1>Clock</h1>
+      {/* <p>{`${hours}:${minutes}:${seconds}`}</p> */}
+      <p>{this.state.time.toLocaleTimeString()}</p>
     </div>
     )
   }
