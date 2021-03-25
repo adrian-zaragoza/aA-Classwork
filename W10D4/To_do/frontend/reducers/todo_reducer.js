b91a0284ad59f3fb7f06259895b6d114895cb34b
Justin Cheasty <cheasty@gmail.com>
2021-03-25 15:09:37 -0700

    create action and reducer files

--- W10D4/To_do/frontend/reducers/todo_reducer.js
+++ W10D4/To_do/frontend/reducers/todo_reducer.js
@@ -4,4 +4,12 @@ import {RECEIVE_TODOS} from "../actions/todo_actions"
 const todosReducer = (state = {}, action)=>{
   Object.freeze(state);
   const nextState = Object.assign({}, state)
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+
 }
+
+export default todosReducer;
