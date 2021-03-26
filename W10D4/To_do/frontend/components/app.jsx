d105e846c6bc9f180891a0a0e57d972d4f6159d4
Justin Cheasty <cheasty@gmail.com>
2021-03-26 15:00:59 -0700

    complete phase 1

---
+++ W10D4/To_do/frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>I'm inside App</h1>
+    </div>
+  )
+}
+
+export default App;
--- W10D4/To_do/frontend/todo_redux.jsx
+++ W10D4/To_do/frontend/todo_redux.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configureStore from "./store/store";
-import { receiveTodo} from "./actions/todo_actions"
-import { receiveTodos } from "./actions/todo_actions"
-import { removeTodo } from "./actions/todo_actions"
-import { receiveStep} from "./actions/step_actions"
-import { receiveSteps } from "./actions/step_actions"
-import { removeStep } from "./actions/step_actions"
 
+import configureStore from "./store/store";
+
+import { receiveTodo } from "./actions/todo_actions"
+  import { receiveTodos } from "./actions/todo_actions"
+  import { removeTodo } from "./actions/todo_actions"
+
+import { receiveStep } from "./actions/step_actions"
+  import { receiveSteps } from "./actions/step_actions"
+  import { removeStep } from "./actions/step_actions"
+
+import App from "./components/app"
 
 
 
@@ -23,5 +27,5 @@ document.addEventListener("DOMContentLoaded", () => {
   window.removeStep = removeStep;
   window.receiveSteps = receiveSteps;
   
-  ReactDOM.render(<h1>Todos App</h1>, root);
+  ReactDOM.render(<App />, root);
 })
