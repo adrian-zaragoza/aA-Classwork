8881034beb2be4b1528bee6d53089fcd5d1b7017
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-03-29 23:37:55 -0700

    finish form

--- W10D4/To_do/frontend/components/todos/todo_form.jsx
+++ W10D4/To_do/frontend/components/todos/todo_form.jsx
@@ -4,5 +4,37 @@ class TodoForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { id: new Date().getTime(), title: "", body: "", done: false}
+
+    this.updateTitle = this.updateTitle.bind(this);
+    this.updateBody = this.updateBody.bind(this);
+    this.updateDone = this.updateDone.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateTitle(e){
+    this.setState({ title: e.target.value})
+  }
+
+  updateBody(e) {
+    this.setState({ body: e.target.value })
+  }
+
+  updateDone(e) {
+    this.setState({ done: e.target.value })
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+  }
+
+  render(){
+
+    return(
+      <div>
+        {/* <input type="text" value={this.newState(title: )}/> */}
+      </div>
+    )
   }
 }
+
+export default TodoForm;
--- W10D4/To_do/frontend/components/todos/todo_list.jsx
+++ W10D4/To_do/frontend/components/todos/todo_list.jsx
@@ -12,12 +12,20 @@ class TodoList extends React.Component{
 
     return(
       <div>
-        <h1>Todo List</h1>
-        <ul>
-          {
-            this.props.todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)
-          }
-        </ul>
+        <div>
+          <h1>Todo List</h1>
+          <ul>
+            {
+              this.props.todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)
+            }
+          </ul>
+        </div>
+        <div>
+            <h1>New Todo</h1>
+            <ul>
+              <TodoForm/>
+            </ul>
+        </div>
       </div>
     )
   }
